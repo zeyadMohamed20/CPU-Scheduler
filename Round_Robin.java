4d0d1869738fd63d51a3ec9e59f9803827198996
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2023-04-14 01:58:43 +0200

    Update Round_Robin.java

--- Round_Robin.java
+++ Round_Robin.java
@@ -10,6 +10,40 @@ public class Round_Robin extends  Scheduler
     @Override
     public ArrayList<Gantt_Process>get_GanttChart()
     {
-        return ganttChart;
+        ganttChart = new ArrayList<Gantt_Process>();
+        //sorting readyQueue based on the arrival time
+        sort(SORTING_CRITERIA.ARRIVAL_TIME);
+        int currentTime = 0 ;
+        int NumOfContinue = 0 ;
+        int minArrivalProcessIndex = 0;
+        while(!readyQueue.isEmpty())
+        {
+            for(int currentProcessIndex = 0 ; currentProcessIndex < readyQueue.size(); currentProcessIndex++)
+            {
+                if(readyQueue.get(currentProcessIndex).getArrivalTime() > currentTime)
+                {
+                    NumOfContinue++ ;
+                    if (NumOfContinue == readyQueue.size() )
+                    {
+                        Gantt_Process ganttProcess = new Gantt_Process(0, currentTime, readyQueue.get(minArrivalProcessIndex).getArrivalTime());
+                        ganttChart.add(ganttProcess);
+                        currentTime = readyQueue.get(minArrivalProcessIndex).getArrivalTime();
+                    }
+                }
+                else
+                {
+                    //Create gantt process and add it to gantt chart
+                    Gantt_Process ganttProcess = new Gantt_Process(readyQueue.get(currentProcessIndex).getProcessID(), currentTime,  currentTime + quantum);
+                    ganttChart.add(ganttProcess);
+                    //update the current time
+                    currentTime += quantum ;
+                    readyQueue.get(currentProcessIndex).setBurstTime((readyQueue.get(currentProcessIndex).getBurstTime()-quantum));
+                    if ( readyQueue.get(currentProcessIndex).getBurstTime() <= 0 )
+                        readyQueue.remove(currentProcessIndex);
+                    NumOfContinue = 0;
+                }
+            }
+        }
+            return ganttChart;
     }
 }
