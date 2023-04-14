d73f7b42f04a6a5a59db203ca05e10a9ac266958
shimaa204 <shimaarashad142@gmail.com>
2023-04-14 13:10:26 +0200

    edit in the implementaion of the algorithm

--- FCFS.java
+++ FCFS.java
@@ -1,119 +1,121 @@
-package os;
+package program;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
-
+enum SORTING_CRITERIA2
+{
+    ID,
+    START_TIME,
+    END_TIME
+}
 public class FCFS extends  Scheduler
 {
- 
-    private int maxSize;
-  
-    public FCFS() 
-    {
-      
-    }
-
+    private ArrayList<Gantt_Process> IDLE;
+    private ArrayList<Gantt_Process> merge;
    
     public FCFS(ArrayList<Process> readyQueue)
     {
         
         super(readyQueue);
-       
     }
-    
-    public void processInfo()
+    //sorting based on start time of gantt processes line
+    public void sortGantt(SORTING_CRITERIA2 option)
     {
-       
-        int ID,ar,bt;
-        System.out.println("Enter the information of processes: ");
-        for (int i = 0; i < maxSize; i++)
+        Collections.sort(merge, new Comparator<Gantt_Process>()
         {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("enter ID ,ArrrivalTime,BurstTime of p"+(i+1));
-            ID=sc.nextInt();
-            ar=sc.nextInt();
-            bt=sc.nextInt();
-            readyQueue.add(new Process(ID, ar, bt));
-        }
-        sort(SORTING_CRITERIA.valueOf("ARRIVAL_TIME"));
-        
-        
-        
-     }
-    public  ArrayList<Process> FCFS_Execute()
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of processes: "); //Entering the number of processes
-        int n = sc.nextInt();
-        maxSize = n;
-        processInfo();
-       return readyQueue;
-    }
-    public void printInfo()
-    {
-        System.out.println("Process Id\t\tArrival time\t\tBurst time");
-        System.out.println("--------------------------------------------------------------------------------");
-        for (int i = 0; i < maxSize; i++) 
-        {
-            System.out.println("    p"+readyQueue.get(i).getProcessID()+"\t\t\t    "+readyQueue.get(i).getArrivalTime()+"\t\t\t    "+readyQueue.get(i).getBurstTime());
-        }
-    }  
-    
-    
-    @Override
-    public  ArrayList<Gantt_Process> get_GanttChart()
-    {
-        int cumv = readyQueue.get(0).getBurstTime()+readyQueue.get(0).getArrivalTime();
-        ganttChart.add(new Gantt_Process(readyQueue.get(0).getProcessID(), readyQueue.get(0).getArrivalTime(), cumv));
-        
-        for (int i = 1; i < maxSize; i++) {
-            if(readyQueue.get(i).getArrivalTime() > readyQueue.get(i-1).getArrivalTime()){
-                ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),readyQueue.get(i).getArrivalTime(),readyQueue.get(i).getBurstTime()+ readyQueue.get(i).getArrivalTime()));
-            }else{
-                ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),cumv,readyQueue.get(i).getBurstTime()+ cumv));
-                cumv += readyQueue.get(i).getBurstTime();
+            @Override
+            public int compare(Gantt_Process gp1, Gantt_Process gp2)
+            {
+                switch(option)
+                {
+                    case ID:
+                        return gp1.getProcessId()- gp2.getProcessId();
+                    case START_TIME:
+                        return gp1.getStartTime() - gp2.getStartTime();
+                    case END_TIME:
+                        return gp1.getEndTime()- gp2.getEndTime();
+                    default:
+                        return 0;
+                }
             }
-        }
-        return ganttChart;
+        });
+
     }
-   public void printChart(){
-         System.out.println("\tGantt Chart Line");
-       System.out.println("-------------------------------------------------------------------------------------");
-       System.out.print("time: ");
-       if(readyQueue.get(0).getArrivalTime() != 0){
-             System.out.print("<"+0+"-"+readyQueue.get(0).getArrivalTime()+"> IDLE | ");
-             for (int i = 1; i < maxSize; i++) {
-                    if(readyQueue.get(i).getArrivalTime() > readyQueue.get(i-1).getArrivalTime()){
-                     System.out.print("<"+(readyQueue.get(i-1).getArrivalTime()+readyQueue.get(i-1).getBurstTime())+"-"+readyQueue.get(i).getArrivalTime()+"> IDLE | ");
-                         for (int j = i-1; j < maxSize; j++) {
-                             System.out.print(get_GanttChart().get(j).toString());
-                         }
-                         System.out.println();
-                    }else{
-                         for (int j = 0; j < maxSize; j++) {
-                             System.out.print(get_GanttChart().get(j).toString());
-                         }
-                         System.out.println();
-                    }
-        }
-       }else{
-        for (int i = 1; i < maxSize; i++) {
-            if(readyQueue.get(i).getArrivalTime() > readyQueue.get(i-1).getArrivalTime()){
-             System.out.print("<"+(readyQueue.get(i-1).getArrivalTime()+readyQueue.get(i-1).getBurstTime())+"-"+readyQueue.get(i).getArrivalTime()+"> IDLE | ");
-                 for (int j = i-1; j < maxSize; j++) {
-                     System.out.print(get_GanttChart().get(j).toString());
+
+
+    
+ public ArrayList<Gantt_Process>get_GanttChart(){
+         IDLE = new ArrayList<Gantt_Process>();
+         merge = new ArrayList<Gantt_Process>();
+         ganttChart = new ArrayList<Gantt_Process>();
+     
+         sort(SORTING_CRITERIA.valueOf("ARRIVAL_TIME"));
+        int c=0;
+
+        int cumv = readyQueue.get(0).getBurstTime()+readyQueue.get(0).getArrivalTime();
+        if(readyQueue.get(0).getArrivalTime() > 0){
+            IDLE.add(new Gantt_Process(0,0, readyQueue.get(0).getArrivalTime()));
+            ganttChart.add(new Gantt_Process(readyQueue.get(0).getProcessID(),readyQueue.get(0).getArrivalTime(),cumv));
+             for (int i = 1; i < readyQueue.size(); i++) 
+             {
+                 c = readyQueue.get(i-1).getArrivalTime()+readyQueue.get(i-1).getBurstTime();
+                 
+                 if(readyQueue.get(i).getArrivalTime()> c)
+                 {
+                     IDLE.add(new Gantt_Process(0,c,readyQueue.get(i).getArrivalTime()));
+                     
+                     cumv = readyQueue.get(i).getArrivalTime();
+                     ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),cumv ,readyQueue.get(i).getBurstTime()+ cumv));
+                   
+                    
+                 }else{
+                     cumv = readyQueue.get(i).getArrivalTime();
+                     ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),cumv,readyQueue.get(i).getBurstTime()+ cumv));
+                  
+
                  }
-                 System.out.println();
-             }else{
-                 for (int j = 0; j < maxSize; j++) {
-                     System.out.print(get_GanttChart().get(j).toString());
+
+             }
+        }else{
+             ganttChart.add(new Gantt_Process(readyQueue.get(0).getProcessID(),readyQueue.get(0).getArrivalTime(),cumv));
+             for (int i = 1; i < readyQueue.size(); i++) 
+             {
+                 c = readyQueue.get(i-1).getArrivalTime()+readyQueue.get(i-1).getBurstTime();
+                 
+                 if(readyQueue.get(i).getArrivalTime()> c)
+                 {
+                     IDLE.add(new Gantt_Process(0,c,readyQueue.get(i).getArrivalTime()));
+                     
+                     cumv = readyQueue.get(i).getArrivalTime();
+                     ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),cumv ,readyQueue.get(i).getBurstTime()+ cumv));
+                   
+                    
+                 }else{
+                     cumv = readyQueue.get(i).getArrivalTime();
+                     ganttChart.add(new Gantt_Process(readyQueue.get(i).getProcessID(),cumv,readyQueue.get(i).getBurstTime()+ cumv));
+                  
+
                  }
-                 System.out.println();
+
              }
         }
-       }
-   }
-    
+        for (int i = 0; i < IDLE.size(); i++) {
+            merge.add(IDLE.get(i));
+            
+        }
+        for (int i = 0; i < ganttChart.size(); i++) {
+            merge.add(ganttChart.get(i));
+            
+        }
+         sortGantt(SORTING_CRITERIA2.valueOf("START_TIME"));
+        return merge;
+    }
+ 
+
 }
+    
+
       
 
  
